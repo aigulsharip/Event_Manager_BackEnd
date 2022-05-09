6656fb4ccd437e06730af570ccb7ac171e081b05
SyrymAlisher <syrymalisher@gmail.com>
2022-05-09 18:16:07 +0600

    branch and comments proj created

---
+++ comment-api/src/test/java/kz/daracademy/commentapi/CommentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.daracademy.commentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
