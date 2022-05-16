1dfa59435479840fed4fb8d631435df96440043b
abylai <a.zhalgasbai@csi.kz>
2022-05-16 10:51:47 +0600

    created mail-notification-api

---
+++ mail-notification/src/main/java/com/dar/notification/MailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.dar.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailNotificationApplication.class, args);
+    }
+
+}
