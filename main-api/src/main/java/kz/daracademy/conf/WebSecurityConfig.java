c4f590965fa3b8af62cd370bccee0c547c2ed6f4
aigulsharip <aigul.sharip@nu.edu.kz>
2022-05-25 14:31:41 +0600

    added main-api

---
+++ main-api/src/main/java/kz/daracademy/conf/WebSecurityConfig.java
@@ -0,0 +1,72 @@
+package kz.daracademy.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http = http.cors().and().csrf().disable();
+
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(
+                                    HttpServletResponse.SC_UNAUTHORIZED,
+                                    ex.getMessage()
+                            );
+                        }
+                )
+                .and();
+
+        http
+                // Our public endpoints
+                .authorizeRequests()
+                .antMatchers("/healthcheck").permitAll()
+                .antMatchers(
+                        "/swagger-ui/**",
+                        "/swagger/**",
+                        "/api-docs.json/**"
+                ).permitAll()
+                // Our private endpoints
+                .anyRequest().authenticated();
+
+        // Add JWT token filter
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+    }
+
+
+}
