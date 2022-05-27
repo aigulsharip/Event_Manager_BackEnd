6427a9dacda508dacff575000605ac79dad11339
Your Name <yeyessenov@dar.kz>
2022-05-27 21:16:53 +0600

    get user jwt

--- main-api/src/main/java/kz/daracademy/controller/MainController.java
+++ main-api/src/main/java/kz/daracademy/controller/MainController.java
@@ -1,8 +1,13 @@
 package kz.daracademy.controller;
 
+import kz.daracademy.model.UserDetailsModel;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/main")
@@ -13,4 +18,10 @@ public class MainController {
         return "Hello! It's work!";
     }
 
+    @GetMapping("/user")
+    public UserDetailsModel getUserByJWT() {
+        UserDetailsModel principal = (UserDetailsModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return principal;
+    }
 }
---
+++ main-api/src/main/java/kz/daracademy/model/UserDetailsModel.java
@@ -0,0 +1,24 @@
+package kz.daracademy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsModel {
+
+    private String email;
+    private String id;
+    private List<String> roles;
+    private String userId;
+    private HashMap<String, ArrayList<String>> acl;
+
+}
